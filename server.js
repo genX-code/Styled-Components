2bf601bc1d0ad07392304e5153b06b9a09ab600b
genX-code <toycampani@gmail.com>
2019-01-07 22:10:40 +0800

    Prepare for deploy

--- server.js
+++ server.js
@@ -14,11 +14,12 @@ twitterProxyServer({
 
 app.use(cors());
 app.use("/tweets", route);
+console.log(process.env.NODE_ENV);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
-  app.get("*", (req, res) => {
+  app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html" ))
   })
 }
