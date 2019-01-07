fad150c456976a5c8be995819a788301e9daa045
genX-code <toycampani@gmail.com>
2019-01-07 21:21:23 +0800

    Prepare heroku deploy

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,53 +1,56 @@
 const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-  entry: "./src/app.js",
-  output: {
-    path: path.resolve(__dirname, "build"),
-    filename: "bundle.js"
-  },
-
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        loader: "babel-loader"
-      },
-
-      {
-        test: /\.s?css$/,
-        use: [MiniCssExtractPlugin.loader,"css-loader", "sass-loader", "image-webpack-loader"]
-      },
-
-      {
-        test: /\.(gif|png|jpe?g|svg)$/i,
-        use: [
-          'file-loader',
-          {
-            loader: 'image-webpack-loader',
-            options: {
-              bypassOnDebug: true, // webpack@1.x
-              disable: true, // webpack@2.x and newer
+module.exports = env => {
+  return {
+    entry: "./src/app.js",
+    output: {
+      path: path.resolve(__dirname, "build"),
+      filename: "bundle.js"
+    },
+  
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          loader: "babel-loader"
+        },
+  
+        {
+          test: /\.s?css$/,
+          use: [MiniCssExtractPlugin.loader,"css-loader", "sass-loader", "image-webpack-loader"]
+        },
+  
+        {
+          test: /\.(gif|png|jpe?g|svg)$/i,
+          use: [
+            'file-loader',
+            {
+              loader: 'image-webpack-loader',
+              options: {
+                bypassOnDebug: true, // webpack@1.x
+                disable: true, // webpack@2.x and newer
+              },
             },
-          },
-        ],
-      }
+          ],
+        }
+      
+      ]
+    },
+  
+    mode: "production",
+  
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: "style.css",
+      })
+    ],
     
-    ]
-  },
-
-  mode: "development",
-
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: "style.css",
-    })
-  ],
-
-  devServer: {
-    historyApiFallback: true,
-    contentBase: path.join(__dirname, 'build'),
-    port: 9000
+  
+    devServer: {
+      historyApiFallback: true,
+      contentBase: path.join(__dirname, 'build'),
+      port: 9000
+    }
   }
 }
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const port = process.env.PORT || 3000;
 const route = require("./route");
 const { key, secret } = require("./config");
 const cors = require("cors");
+const path = require("path");
 
 const twitterProxyServer = require('twitter-proxy');
 twitterProxyServer({
@@ -13,6 +14,14 @@ twitterProxyServer({
 
 app.use(cors());
 app.use("/tweets", route);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html" ))
+  })
+}
 
 
 app.listen(port, () => console.log(`Server is listening on ${port}`) )
