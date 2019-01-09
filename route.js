cbec33f208daef06cc0a1d231452e47ead918544
genX-code <toycampani@gmail.com>
2019-01-10 00:36:28 +0800

    Add status codes

--- route.js
+++ route.js
@@ -3,19 +3,19 @@ const axios = require("axios");
 
 router.get("/", (req, res) =>{
   axios.get("http://localhost:7890/1.1/statuses/user_timeline.json\?count\=30\&screen_name\=makeschool")
-    .then(response => res.json(response.data))
+    .then(response => res.status(200).json(response.data))
     .catch(err => console.log(err))
 })
 
 router.get("/ycomb", (req, res) =>{
   axios.get("http://localhost:7890/1.1/statuses/user_timeline.json\?count\=30\&screen_name\=ycombinator")
-    .then(response => res.json(response.data))
+    .then(response => res.status(200).json(response.data))
     .catch(err => console.log(err))
 })
 
 router.get("/news", (req, res) =>{
   axios.get("http://localhost:7890/1.1/statuses/user_timeline.json\?count\=30\&screen_name\=newsycombinator")
-    .then(response => res.json(response.data))
+    .then(response => res.status(200).json(response.data))
     .catch(err => console.log(err))
 })
 
